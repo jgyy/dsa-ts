791a4067b1be05f3c3aeec8c33087479b6f0dc33
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2023-02-04 11:59:35 +0800

    done with intro

---
+++ 05-intro/power.ts
@@ -0,0 +1,6 @@
+console.log(Math.pow(2, 3));
+console.log(Math.pow(2, 10)); 
+console.log(Math.pow(2, 100)); 
+console.log(Math.pow(2, 1000));
+console.log(Math.pow(2, 1023));
+console.log(Math.pow(2, 1024)); 
